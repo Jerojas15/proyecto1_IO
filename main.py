c80dc7c2fb14f130e5f8425a0536d9f1f35125b0
Jose Paulo Yock Fuentes <jyock1997@gmail.com>
2017-09-14 11:00:26 -0600

    Clase numero la cual contiene a M

---
+++ main.py
@@ -0,0 +1,18 @@
+from number import Number as n
+
+n1 = n(1,2)
+n2 = n(2,3)
+
+print(n1+n2)
+print(n1-n2)
+print(n1*n2)
+print(n1/n2)
+print()
+n1 += n2
+print(n1)
+n1 -= n2
+print(n1)
+n1 *= n2
+print(n1)
+n1 /= n2
+print(n1)
---
+++ number.py
@@ -0,0 +1,77 @@
+
+class Number:
+
+	def __init__(self, n=0, M=0):
+		self.n = n
+		self.M = M
+
+	def __str__(self):
+		s = ""
+		if(self.n == 0 and self.M == 0):
+			return "0"
+
+		if(self.n != 0):
+			s += str(self.n)
+		if(self.M != 0):
+			if(self.M > 0):
+				s+= "+"
+			s += str(self.M) + "M"
+
+		return s
+
+
+	def __lt__(self, other):
+
+		if(self.M == other.M):
+			return self.n < other.n
+		return self.M < other.M
+
+	def __add__(self, other):
+		newN = self.n + other.n
+		newM = self.M + other.M
+
+		return Number(newN, newM)
+
+	def __iadd__(self, other):
+		self.n += other.n
+		self.M += other.M
+
+		return self
+
+	def __sub__(self, other):
+		newN = self.n - other.n
+		newM = self.M - other.M
+		
+		return Number(newN, newM)
+
+	def __isub__(self, other):
+		self.n -= other.n
+		self.M -= other.M
+		
+		return self
+
+	def __mul__(self, other):
+		newN = self.n * other.n
+		newM = self.M * other.M
+
+		return Number(newN, newM)
+
+	def __mul__(self, other):
+		print("entro")
+		self.n *= other.n
+		self.M *= other.M
+
+		return self
+
+	def __truediv__(self, other):
+		print("entro")
+		newN = self.n / other.n
+		newM = self.M / other.M
+
+		return Number(newN, newM)
+
+	def __truediv__(self, other):
+		self.n /= other.n
+		self.M /= other.M
+
+		return self
